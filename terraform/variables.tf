5917b8aab073803d628dc339bccaf41c6a711fbc
Tiger4Code <light.email.work@gmail.com>
2025-02-04 20:44:09 -0500

    add github actions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,22 +9,15 @@ variable "region" {
   description = "AWS Region"
   type        = string
 
-  default = "us-east-1"
+  default = null 
 }
 
 variable "ecr_repository" {
   description = "ECR Repository"
   type        = string
 
-  default = "noor_k8s_tutorial_repo"
+  default = null
 }
-
-# variable "ecr_repositories" {
-#   description = "A list of ECR repository names. The first item in the list represents the repository for the dynamic website image (used for Docker image builds)."
-#   type        = map(string)
-
-#   default = {}
-# }
 
 variable "eks_cluster_version" {
   description = "Version of kubernetes running on cluster"
