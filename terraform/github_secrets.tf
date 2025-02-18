e8353aa33243ad532fa85135980d10c15ffb7978
Tiger4Code <light.email.work@gmail.com>
2025-02-17 21:04:25 -0500

    gha public ecr

--- terraform/github_secrets.tf
+++ terraform/github_secrets.tf
@@ -7,6 +7,7 @@ resource "null_resource" "set_github_secrets" {
       gh secret set AWS_ACCOUNT_ID --body="$(terraform output -raw aws_account_id)"
       gh secret set AWS_REGION --body="$(terraform output -raw aws_region)"
       gh secret set ECR_REPOSITORY --body="$(terraform output -raw ecr_repo_name)"
+      gh secret set ECR_PUBLIC_REGISTRY_ALIAS --body="$(terraform output -raw ecr_public_registry_alias)"
       gh secret set DOCKER_IMG_TAG --body="$(terraform output -raw docker_img_name)"
     EOT
   }
